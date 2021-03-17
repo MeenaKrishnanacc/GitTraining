a62b993b508c598b517e27c4564e7c6f5476cf63
Meena Krishnan <meena.krishnan@acc.co.nz>
2021-03-18 12:53:53 +1300

    change 1 - feature branch1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World1");
-        System.out.println("Master Three way merge");
+        System.out.println("change 1 - Feature Branch");
+
     }
 }
